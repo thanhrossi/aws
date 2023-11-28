df9278eb629f9b43a525e4a1a2bb9d23941932ad
Tran Thanh <thanhrossi.tt@gmail.com>
2023-11-29 00:14:26 +0700

    Update test.tf
    okay

--- test.tf
+++ test.tf
@@ -10,8 +10,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAQLOB2R4OSLJFSH7Q"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 resource "aws_s3_bucket" "bucket" {
   bucket = "thanhtv0bucket"
