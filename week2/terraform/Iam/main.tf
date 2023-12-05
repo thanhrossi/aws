8b42b3d896f57422ce7f65e93e211a4eb8dbf177
thanhtv36 <v.thanhtv36@vinfast.vn>
2023-12-05 14:54:43 +0700

    assign iam to iam group

--- week2/terraform/Iam/main.tf
+++ week2/terraform/Iam/main.tf
@@ -1,8 +1,22 @@
-resource "aws_iam_group" "example" {
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_group" "group" {
   name = "thanhtv36-group"
 }
 
 resource "aws_iam_group_policy_attachment" "example" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-  group      = aws_iam_group.example.name
+  group      = aws_iam_group.group.name
+}
+
+resource "aws_iam_user" "user" {
+  name = "iamec2"
+}
+
+resource "aws_iam_group_membership" "membership" {
+  name  = aws_iam_group.group.name
+  users = [aws_iam_user.user.name]
+  group = aws_iam_group.group.name
 }
